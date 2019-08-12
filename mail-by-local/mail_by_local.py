f66fa711be06376256916c96b459faa9f27cdd64
Ningbo <365220121@qq.com>
2019-08-12 11:57:13 +0800

    Update mail_by_local.py

--- mail-by-local/mail_by_local.py
+++ mail-by-local/mail_by_local.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         smtp_host = 'localhost'
         port = 25 
         sender_address ='root'
-        receiver_address = 'yangningbo@huawei.com;shine.zhangqiang@huawei.com'
+        receiver_address = 'yangningbo@xxx.com;yangningbo1@xxx.com'
         cc_receiver_address = ''
         subject='python test from yangjian'
         content='弱密码-'+str(time.asctime(time.localtime(time.time())))
